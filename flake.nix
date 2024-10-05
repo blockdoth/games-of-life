7535366a5f59f2082bff4fcd69f37d299c82459f
blockdoth <pepijn.pve@gmail.com>
2024-10-05 22:13:50 +0200

    working on adding rust, it has been a pain so far

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A nix flake";
+  description = "Game of life in various languages";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -16,8 +16,19 @@
     in {
       packages = forEachSupportedSystem (system: let
         pkgs = system.pkgs;
-      in {
-        pyraylib = pkgs.python3Packages.buildPythonPackage rec {
+        in rec {
+        raylib-source = pkgs.fetchFromGitHub{
+          owner = "raysan5";               
+          repo = "raylib";                
+          rev = "ae50bfa2cc569c0f8d5bc4315d39db64005b1b08";                  
+          sha256 = "sha256-gEstNs3huQ1uikVXOW4uoYnIDr5l8O9jgZRTX1mkRww="; 
+        };
+
+        raylib = pkgs.raylib.overrideAttrs (oldAttrs: {
+          src = raylib-source;
+        });
+        
+        pyraylib = pkgs.python3Packages.buildPythonPackage {
           pname = "pyraylib";
           version = "1.0.2";
 
@@ -43,6 +54,63 @@
             export PKG_CONFIG_PATH=${pkgs.raylib}/lib/pkgconfig
           '';
         };
+
+        rustraylib = pkgs.rustPlatform.buildRustPackage {
+          pname = "rustraylib";
+          version = "5.5.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "raylib-rs";
+            repo = "raylib-rs";
+            rev = "edbfe848313d8d28250d12c2f5ec1ad5fbaa8adb";
+            sha256 = "sha256-16TAKT8VOT+bmIUqsOtqBnsC4prOYCi7QB+1IfgVSuw=";
+          };
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+            clang
+            cargo
+            rustc  
+          ];
+
+          buildInputs = with pkgs; [
+            mesa 
+            glfw
+            alsa-lib            
+            wayland
+            wayland-protocols
+            libxkbcommon
+            xorg.libX11
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+          ];
+
+          cargoPatches = [
+            ./rust/patches/cargo.lock.patch    #The library doesnt come with a cargo lock
+            ./rust/patches/fix-examples.patch  #One specific example wouldnt build, removed it from the cargo.toml
+          ];
+
+          doCheck = false;
+          cargoHash = "sha256-oTwrf7TpTtVadgk66tUDuiq08/vRhOD3G9NUSocpjfk="; 
+          
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          CMAKE_PREFIX_PATH= "${pkgs.glfw}/lib/cmake/glfw3:$CMAKE_PREFIX_PATH";
+          OPENGL_INCLUDE_DIR="${pkgs.mesa.dev}/include";
+
+          buildPhase = ''
+            rm ./raylib-sys/raylib -r
+            cp -r ${raylib-source} ./raylib-sys/raylib
+            cargo build --release --features "wayland"
+          '';    
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r ./target/release/ $out/bin
+          '';
+        };
+
         javaraylib = pkgs.maven.buildMavenPackage {
           pname = "javaraylib";
           version = "0.0.3";
@@ -66,12 +134,14 @@
 
 
         };
-
-      });
+        }
+      );
 
       devShells = forEachSupportedSystem (system: let
         pkgs = system.pkgs;
+        raylib = self.packages.${pkgs.system}.raylib;
         pyraylib = self.packages.${pkgs.system}.pyraylib;
+        rustraylib = self.packages.${pkgs.system}.rustraylib;
         # javaraylib = self.packages.${pkgs.system}.javaraylib;
       in {
         default = pkgs.mkShell {
@@ -81,14 +151,18 @@
             echo 'Python interpreter lives at ${pkgs.python3Packages.python}'
             echo 'JDK lives at ${pkgs.jdk22}'
             echo 'pyraylib lives at ${pyraylib}'
+            echo 'rustraylib lives at ${rustraylib}'
           '';
 
+
           packages = with pkgs; [
+            cargo
+            rustc
             raylib
             jdk22
+            rustraylib
             # javaraylib
           ] ++ (with pkgs.python3Packages; [
-            pypy
             python
             pyraylib
             cffi
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # time python ./python/main.py < config
-time pypy ./python/main.py < config
+# time pypy ./python/main.py < config
 # time cc ./c/main.c -l raylib -o ./c/main && ./c/main < config
+rustc -o ./rust/main ./rust/main.rs --extern raylib=
